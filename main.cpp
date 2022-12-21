ff7285c87ba3ae82213edd0a84fb1a2acf9e50b6
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-21 22:31:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+double f(double x,double &fx){
+    fx=x*x*cos(x)+1;
+    return fx;
+}
 
 int main() {
+   double a=0, b=0, x=0, fx, err;
+    while (f(a, fx) * f(b, fx) >= 0) {
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+        if (f(a, fx) * f(b, fx) < 0) {
+            break;
+        }
+    }
+    while (f(x,fx) != 0) {
+        x = (a + b) / 2;
+
+        if (f(a, fx) * f(b, fx) < 0) {
+            a = x;
+        } else {
+            b = x;
+        }
+        err = abs((b - a)) / 2;
+        if (err < 1e-6) {
+            break;
+        }
+    }
+    cout << x << endl;
+
    return 0;
 }
