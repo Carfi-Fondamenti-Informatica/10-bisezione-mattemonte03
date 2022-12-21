7a38b72797a2973c0a5d5577f918eddb20dbfe42
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-21 22:49:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-double f(double x , double &fx)
+double f(double y , double &fy)
 {
-    fx = x * x * cos(x) + 1;
-    return fx;
+    fy = y * y * cos(y) + 1;
+    return fy;
 }
 
 int main() {
 double a=0,b=0,fx=0,x=0,err=2;
 int g=0;
-while(f(a,fx)*f(b,fx)>=0) {
+while(f(a,fy)*f(b,fy)>=0) {
     cout <<"inserire estremi"<<endl;
     cin >> a >> b;
 
-    if (f(a,fx)*f(b,fx) < 0){
+    if (f(a,fy)*f(b,fy) < 0){
         break;
     }
 }
 
-while (f(x,fx)!=0) {
+while (f(x,fy)!=0) {
     x=(a+b)/2;
-        if (f(x, fx) * f(b, fx) < 0) {
-            a = x;
+        if (f(a, fy) * f(b, fy) < 0) {
+            a = y;
         }
         else{
-        b = x;
+        b = y;
         }
     err = abs((a - b)) / 2;
         if (err < 1e-6){
             break;}
 }
 
-cout<< x;
+y=y*10000;
+y=(int)y;
+y=y/10000;
+cout<< y;
 
 
 
